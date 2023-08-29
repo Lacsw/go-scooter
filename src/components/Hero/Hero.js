d0cdf2594c4b9483c27c2afb5764937b886dbb8b
Roman Frolov <frolloff1@yandex.ru>
2023-08-29 20:58:56 +0300

    feat: layout hero

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,34 @@
+import './Hero.css';
+
+import logo from '../../images/logo.svg';
+import heroImage from '../../images/hero-image.png';
+import heroImageCaption from '../../images/image-caption.png';
+
+export default function Hero() {
+  return (
+    <section className='hero'>
+      <div className='hero__container'>
+        <img className='hero__logo' src={logo} alt='логотип' />
+        <div className='hero__image-group'>
+          <img className='hero__image' src={heroImage} alt='скутер' />
+          <img
+            className='hero__image-caption'
+            src={heroImageCaption}
+            alt='сообщество любителей самокатов'
+          />
+          <span className='hero__image-ellipse'></span>
+        </div>
+
+        <h1 className='hero__title'>Вступайте в клуб Самокатов</h1>
+        <p className='hero__subtitile'>
+          Купите абонемент, чтобы стать частью клуба в новом сезоне–2024. 8
+          месяцев за
+          <span className='hero__new-price'>
+            499₽<span className='hero__old-price'>3190₽</span>
+          </span>
+        </p>
+        <button className='hero__btn'>Стать частью клуба</button>
+      </div>
+    </section>
+  );
+}
